cd4217fc568d2284901ef19eedc04ceb3c92572a
breeze <819974025@qq.com>
2020-03-20 22:43:55 +0800

    breeze：冒泡排序的优化

--- src/main/java/com/breeze/sort/BubbleSort.java
+++ src/main/java/com/breeze/sort/BubbleSort.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
  */
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {3, 9, -1, 10, -2};
+        int[] arr = {3, 9, -1, 10, 20};
 
         //为了容易理解，我们把冒泡排序的演变过程展示出来
 
@@ -74,20 +74,28 @@ public class BubbleSort {
     }
 
     private static void sort() {
-        int[] arr = {3, 9, -1, 10, -2};
+        int[] arr = {3, 9, -1, 10, 20};
         //临时变量
         int temp = 0;
 
         for (int i = 1; i < arr.length; i++) {
+            //优化算法的变量 - 是否进行过交换
+            boolean flag = true;
             for (int j = 0; j < arr.length - i; j++) {
                 if (arr[j] > arr[j+1]) {
+                    flag = false;
                     temp = arr[j+1];
                     arr[j+1] = arr[j];
                     arr[j] = temp;
                 }
             }
-            System.out.print("优化后：第" + (i+1) + "次排序的数组：");
+            System.out.print("优化后：第" + i + "次排序的数组：");
             System.out.println(Arrays.toString(arr));
+
+            //如果一次交换都没有发生
+            if (flag) {
+                break;
+            }
         }
     }
 }
